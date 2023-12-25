6b5f72de85194ac76373f1a2866ea256ac11a413
İSMAİL TAHA KORKUT <taha_ksk35@hotmail.com>
2023-12-25 12:14:51 +0300

    sda
    ada

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ const { registerUser, loginUser, getUserList, newPasswordUser, logoutUser, delet
 const { protect } = require('./modules/auth');
 
 const { createMeeting, updateMeeting,getMeeting } = require('./handlers/meeting_handler');
-const { createMessage, getMessage } = require('./handlers/message_handler');
 
-app.post('/create-message', createMessage);
-app.get('/get-message', createMessage);
 
 
 app.get('/getInstagram', (req, res) => {
